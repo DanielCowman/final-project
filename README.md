b71e3afb7a1ef9b1bb0e3587d90d86700941fcfa
DanielCowman <dcowman@mail.valenciacollege.edu>
2016-04-26 17:50:33 -0400

    code cleanup

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 
 **Live Currency Converter**
 
-* You do not need any third party packages
+* You do not need to install any third party packages
 
-* clone the repo and run main.py
+* clone the repo and run CurrencyRunner.py
 
 * Enter the country code you want to switch money from and to, then enter the amount of money you would like to convert. 
--- devel/main.py
+++ devel/main.py
@@ -1,25 +1,22 @@
 import requests
 
-# base URL for fixer.io
-JSON_URL = 'http://api.fixer.io/latest?base='
 # currency table
 CURRENCY_TABLE = \
     {
-        "USD": 'US Dollar', "GBP": 'Great British Pound', "CAD": 'Canadian dollar', "AUD": 'Australian dollar',
-        "BGN": 'Bulgarian lev', "BRL": 'Brazillian real', "CHF": 'Swiss franc', "TRY": 'Turkish lira',
-        "CNY": 'Chinese yuan', "CZK": 'Czech koruna', "DKK": 'Danish krone', "HKD": 'Hong Kong dollar',
-        "HRK": 'Croatian kuna', "HUF": 'Hungarian forint', "IDR": 'Indonesian rupiah', "ZAR": 'South African rand',
-        "ILS": 'Israeli new shekel', "INR": 'Indian rupee', "JPY": 'Japanese yen', "KRW": 'South Korean won',
-        "MXN": 'Mexican peso', "MYR": 'Malaysian ringgit', "NOK": 'Norweigian krone', "NZD": 'New Zealand dollar',
-        "PHP": 'Phillippine peso', "PLN": 'polish zloty', "RON": 'Romanian new Leu', "RUB": 'Russian ruble',
-        "SEK": 'Swedish krona', "SGD": 'Singapore dollar', "THB": 'Thai baht'
+        "USD": "US Dollar", "GBP": "Great British Pound", "CAD": "Canadian dollar", "AUD": "Australian dollar",
+        "BGN": "Bulgarian lev", "BRL": "Brazilian real", "CHF": "Swiss franc", "TRY": "Turkish lira",
+        "CNY": "Chinese yuan", "CZK": "Czech koruna", "DKK": "Danish krone", "HKD": "Hong Kong dollar",
+        "HRK": "Croatian kuna", "HUF": "Hungarian forint", "IDR": "Indonesian rupiah", "ZAR": "South African rand",
+        "ILS": "Israeli new shekel", "INR": "Indian rupee", "JPY": "Japanese yen", "KRW": "South Korean won",
+        "MXN": "Mexican peso", "MYR": "Malaysian ringgit", "NOK": "Norwegian krone", "NZD": "New Zealand dollar",
+        "PHP": "Philippine peso", "PLN": "polish zloty", "RON": "Romanian new Leu", "RUB": "Russian ruble",
+        "SEK": "Swedish krona", "SGD": "Singapore dollar", "THB": "Thai baht"
     }
 
 
-def get_user_variables():
-    print('Welcome the the currency conversion tool!')
-    print('Please locate the countries currency codes from the table below:')
-    num = 0
+def print_startup():
+    print("Welcome the the currency conversion tool!")
+    print("Please locate the countries currency codes from the table below:")
 
     print("| Code | Country | ")
     # Loops through the currency table and prints the codes with their country
@@ -27,14 +24,17 @@ def get_user_variables():
         print(code + ": " + CURRENCY_TABLE[code])
 
 
-def pull_json_data_and_print_result(url):
+def convert_currency():
+    # run the print starup function above
+    print_startup()
     # asks the user for the currency codes for both countries that will be used within the program.
     currency_code_from = input("Enter the CURRENCY CODE you would like to convert FROM:")
     currency_code_to = input("Enter the CURRENCY CODE you would like to convert TO:")
     amount_money = input("Enter the AMOUNT of money you would like to convert (Enter integers/numbers only):")
+    url = "http://api.fixer.io/latest?base="
 
     # crafts the URL
-    crafted_url = url + currency_code_from + '&symbols=' + currency_code_to
+    crafted_url = url + currency_code_from + "&symbols=" + currency_code_to
     # pulls the data from fixer.io and returns it as a dictionary
     response = requests.request("GET", crafted_url).json()
     # find the sum of the money and the current rate of the currency
@@ -46,12 +46,3 @@ def pull_json_data_and_print_result(url):
     print("| CURRENT RATE: " + str(response["rates"][currency_code_to]) + " |")
 
     print("You will receive: " + str(converted_total) + " " + CURRENCY_TABLE[currency_code_to])
-
-
-def main():
-    # greet the user and print out the code to country table.
-    get_user_variables()
-    pull_json_data_and_print_result(JSON_URL)
-
-
-main()
